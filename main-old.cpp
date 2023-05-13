6816fc66d343fb3eb41739fba49398ae94d430df
foxcane <mihailuozuhov@gmail.com>
2023-05-13 11:48:55 +0400

    Added the ability to view specific photos

--- main-old.cpp
+++ main-old.cpp
@@ -14,6 +14,9 @@
 
 /* access(), R_OK, F_OK */
 #ifdef _WIN32
+#include <Windows.h>
+#include <WinUser.h>
+//#include "resource.h"
 #include <io.h>
 #define R_OK 4
 #define F_OK 0
@@ -22,7 +25,6 @@
 
 struct PictureData {
     std::string PicName;
-    sf::Texture PicTexture;
 };
 
 std::vector<PictureData> Pictures;
@@ -40,33 +42,37 @@ bool isPicture(std::string Filename) {
     return Result;
 }
 
-void loadPictures(std::string DirPictures) {
-    const std::filesystem::path CurrentDir{ DirPictures };
-    for (auto const& DirEntry : std::filesystem::directory_iterator{ CurrentDir }) {
+bool loadPictures(std::string DirPictures) {
+    try {
+        const std::filesystem::path CurrentDir{ DirPictures };
+        for (auto const& DirEntry : std::filesystem::directory_iterator{ CurrentDir }) {
 
 #ifdef __linux__
-        std::string Filename = DirEntry.path();
+            std::string Filename = DirEntry.path();
 #endif
 
 #ifdef _WIN32
-        std::string Filename = (DirEntry.path()).string();
+            std::string Filename = (DirEntry.path()).string();
 #endif
 
-        if (ACCESS(Filename.c_str(), R_OK) == -1) {
-            std::cout << Filename << ": permission denied\n";
-            continue;
+            if (ACCESS(Filename.c_str(), R_OK) == -1) {
+                std::cout << Filename << ": permission denied\n";
+                continue;
+            }
+            std::string OriginFilename = Filename;
+            int Count = 0;
+            for (auto Elem : Filename) { Filename[Count] = std::tolower(Elem); Count++; }
+            if (isPicture(Filename)) {
+                PictureData Picture;
+                Picture.PicName = OriginFilename;
+                Pictures.push_back(Picture);
+            }
         }
-        std::string OriginFilename = Filename;
-        int Count = 0;
-        for (auto Elem : Filename) { Filename[Count] = std::tolower(Elem); Count++; }
-        if (isPicture(Filename)) {
-            PictureData Picture;
-            Picture.PicName = OriginFilename;
-            sf::Texture CurTexture;
-            if (CurTexture.loadFromFile(OriginFilename)) { Picture.PicTexture = CurTexture; };
-            Pictures.push_back(Picture);
-        }
+    } catch(...) {
+        std::cout << DirPictures << ": permission denied\n";
+        return false;
     }
+    return true;
 }
 
 float getReductionFactor(sf::Sprite& CurSprite) {
@@ -83,10 +89,9 @@ float getReductionFactor(sf::Sprite& CurSprite) {
     return ReductionFactor;
 }
 
-sf::Vector2u resizePicture(sf::Sprite &CurSprite) {
-    float ReductionFactor = getReductionFactor(CurSprite);    
+sf::Vector2u resizePicture(sf::Sprite& CurSprite) {
+    float ReductionFactor = getReductionFactor(CurSprite);
     if (ReductionFactor > 1.0) ReductionFactor = 1.0;
-    if (ReductionFactor < 1.0) std::cout << "Picture bigger than screen!\n";
     sf::FloatRect CurSpriteSize = CurSprite.getGlobalBounds();
     CurSprite.setScale(ReductionFactor, ReductionFactor);
     sf::Vector2u newSize;
@@ -96,107 +101,93 @@ sf::Vector2u resizePicture(sf::Sprite &CurSprite) {
 }
 
 void displayPicture(size_t CurSpriteNum, sf::RenderWindow& Window) {
-    sf::Sprite CurSprite(Pictures[CurSpriteNum].PicTexture);
+    sf::Texture CurTexture;
+    if (!CurTexture.loadFromFile(Pictures[CurSpriteNum].PicName)) {
+        std::cout << "Picture '" << Pictures[CurSpriteNum].PicName << "' can't be displayed\n" << "\n";
+        return;
+    }
+    sf::Sprite CurSprite(CurTexture);
     sf::FloatRect CurSpriteSize = CurSprite.getGlobalBounds();
-    unsigned int CurSpriteWidth = CurSpriteSize.width;
-    unsigned int CurSpriteHeight = CurSpriteSize.height;
-
+    unsigned int CurSpriteWidth = static_cast<unsigned int>(CurSpriteSize.width);
+    unsigned int CurSpriteHeight = static_cast<unsigned int>(CurSpriteSize.height);
     do {
         sf::Vector2u pictureSize = resizePicture(CurSprite);
         CurSpriteWidth = pictureSize.x;
         CurSpriteHeight = pictureSize.y;
-    } while((CurSpriteWidth > ScreenInfo.width) || (CurSpriteHeight > ScreenInfo.height));
-
+    } while ((CurSpriteWidth > ScreenInfo.width) || (CurSpriteHeight > ScreenInfo.height));
     Window.create(sf::VideoMode(CurSpriteWidth, CurSpriteHeight), Pictures[CurSpriteNum].PicName, sf::Style::Close);
     Window.setVisible(false);
     Window.setPosition(sf::Vector2i(sf::VideoMode::getDesktopMode().width / 2 - Window.getSize().x / 2, sf::VideoMode::getDesktopMode().height / 2 - Window.getSize().y / 2));
     Window.setVisible(true);
     Window.draw(CurSprite);
     Window.display();
+    /*
+    *
+    * how to set an icon into application window?
+    * I can't do it yet...
+    *
+    #ifdef _WIN32
+        HICON hIcon = LoadIcon(NULL, MAKEINTRESOURCE(IDI_ICON));
+        if (hIcon) {
+            std::string Title = Pictures[CurSpriteNum].PicName;
+            std::cout << Pictures[CurSpriteNum].PicName << "\n";
+            LPCWSTR TitleWin = (LPCWSTR)Title.c_str();
+            HWND hwnd = FindWindow(NULL, TitleWin);
+            SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+        }
+    #endif
+    */
 }
 
 int main(int argc, char* argv[]) {
-    std::string CurDir;
-    if (argc > 1) {
-        if (isPicture(argv[1]) == true) {
-            CurDir = argv[1];
-            PictureData Picture;
-            Picture.PicName = argv[1];
-            sf::Texture CurTexture;
-            if (CurTexture.loadFromFile(Picture.PicName)) Picture.PicTexture = CurTexture;
-            if (errno == 2) return EXIT_FAILURE;
-            Pictures.push_back(Picture);
-        } else if (isPicture(argv[1]) == false) {
-            CurDir = argv[1];
-            try {
-                std::filesystem::directory_entry PATH {CurDir};
-                if (!PATH.exists()) {
-                    std::cout << "Directory '" << CurDir << "' is not found.\n";
-                    return EXIT_FAILURE;
-                }
-                loadPictures(CurDir);
-                if (Pictures.size() == 0) {
-                    std::cout << "Directory '" << CurDir << "' hasn't supported images.";
-                    return EXIT_FAILURE;
-                }
-            } catch(...) {
-                std::cout << "Directory access error";
-                return EXIT_FAILURE;
-            }
-        }
-    } else {
-        CurDir = ".";
-        std::filesystem::directory_entry PATH {CurDir};
-        if (PATH.exists()) loadPictures(CurDir);
-        else { std::cout << "Directory '" << CurDir << "' is not found.\n"; return EXIT_FAILURE; }
-        if (Pictures.size() == 0) {
-            std::cout << "Directory '" << CurDir << "' hasn't supported images.";
-            return EXIT_FAILURE; 
-        }
+    std::string CurDir = ".";
+    if (argc > 1) CurDir = argv[1];
+    std::filesystem::directory_entry entry{ CurDir };
+    if (!entry.exists()) {
+        std::cout << "Directory '" << CurDir << "' is not found.\n";
+        return EXIT_FAILURE;
     }
-    
+    /* prepare images */
+    if (loadPictures(CurDir) == false) return EXIT_FAILURE;
+    if (Pictures.size() == 0) {
+        std::cout << "Directory '" << CurDir << "' hasn't supported images.";
+        return EXIT_FAILURE;
+    }
     /* get screen size */
-    std::cout << Pictures[0].PicName;
     sf::VideoMode Screen;
     ScreenInfo = Screen.getDesktopMode();
-
+    /* common objects */
     sf::RenderWindow Window;
     std::string CurSpriteName = "";
     size_t CurSpriteNum = 0;
-
+    bool PicReady = false;
+    /* create first image window */
     Window.create(sf::VideoMode(1, 1), "");
     displayPicture(0, Window);
-
-    bool StartMoment = true;
-    bool PicReady = false;
-
+    /* window handle */
     while (Window.isOpen()) {
         sf::Event event;
-        if (StartMoment == true) {
-            PicReady = true;
-            StartMoment = false;
-        }
         while (Window.pollEvent(event)) {
             /* close window by mouse */
             if (event.type == sf::Event::Closed) {
                 PicReady = false;
                 Window.close();
             }
-            /* keys pressed handle */
+            /* keys press handle */
             if (event.type == sf::Event::KeyPressed) {
                 /* close window by ESC key */
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                     PicReady = false;
                     Window.close();
                 }
-                if (Pictures.size() > 1) {
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-                        CurSpriteNum++;
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                    CurSpriteNum++;
                     if (CurSpriteNum > Pictures.size() - 1) CurSpriteNum = 0;
-                        PicReady = true; }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-                        CurSpriteNum = (CurSpriteNum == 0) ? Pictures.size() - 1 : CurSpriteNum - 1;
-                        PicReady = true; } 
+                    PicReady = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                    CurSpriteNum = (CurSpriteNum == 0) ? Pictures.size() - 1 : CurSpriteNum - 1;
+                    PicReady = true;
                 }
             }
             if (PicReady == true) {
